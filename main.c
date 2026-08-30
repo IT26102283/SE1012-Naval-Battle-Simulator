0845fa78723f062c84edbc1cfb1cf92b076773c5
IT26102283 <it26102283@my.sliit.lk>
2026-08-30 12:09:03 +0530

    Add battleship intialization & user selection logic

--- main.c
+++ main.c
@@ -27,8 +27,8 @@ typedef struct
 typedef struct
 {
 	char type_code;
-	char type_name;
-	char gun_name;
+	char type_name[30];
+	char gun_name[30];
 	float x, y;
 	float v_max;
 	int is_destroyed;//logically alive or not
@@ -65,6 +65,52 @@ float calculate_max_range(float v_max,float min_angle_deg,float max_angle_deg)
 		}
 	}
 	return max_R;
+}
+
+//Initialize user inputs and Setup battlefir=eld coordinates
+void init_simulation()
+{
+	printf("\n---- SIMULATION SETUP ----\n");
+	printf("Enter Canvas Size D (1-1000) : ");
+	scanf("%f", &canvas_D);
+
+	printf("Enter Battleship Maximum Velocity V_max : ");
+	scanf("%f", &battleship.v_max);
+
+	int b_choice; //for store user choice
+	printf("Select Battleship Type(1-4):\n");
+	printf("1. USS Iowa (BB-61) (U)\n2. MS King George V (M)\n3. Richelieu (R)\n4. Sovetsky Soyuz-class (S)\nSelect your One: ");
+	scanf("%d",&b_choice);
+	
+	//Assign battleship properties based on user selection
+	switch (b_choice)
+	{
+		case 1:
+			battleship.type_code = 'U';
+			snprintf(battleship.type_name, 30, "USS Iowa (BB-61)");
+			snprintf(battleship.gun_name, 30, "50-caliber Mark 7 gun");
+			break;
+		case 2:
+			battleship.type_code = 'M';
+			snprintf(battleship.type_name, 30, "MS King George V");
+			snprintf(battleship.gun_name, 30, "(356mm) Mark VII gun");
+			break;
+		case 3:
+			battleship.type_code = 'R';
+			snprintf(battleship.type_name, 30, "Richelieu");
+			snprintf(battleship.gun_name, 30, "(15 inch) Mle 1935 gun");
+			break;
+		default:
+			battleship.type_code = 'S';
+			snprintf(battleship.type_name, 30, "Sovetsky Soyuz-class");
+			snprintf(battleship.gun_name, 30, "(16 inch) B-37 gun");
+			break;
+	}
+
+	//randomly get battleship position
+	battleship.x = ((float)rand() / RAND_MAX) * canvas_D;
+	battleship.y = ((float)rand() / RAND_MAX) * canvas_D;
+	battleship.is_destroyed = 0; 
 }
 
 
